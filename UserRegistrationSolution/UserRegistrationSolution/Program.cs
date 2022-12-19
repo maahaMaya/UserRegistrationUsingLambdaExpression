8cebc75f30bc6d169570ff967d20297ae15c64a6
SOURAV <devbhsourav@gmail.com>
2022-12-19 22:19:37 +0530

    [sourav]add : folder structure and welcome message

---
+++ UserRegistrationSolution/UserRegistrationSolution/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserRegistrationSolution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome tho the UserRegistrationSolution.");
+        }
+    }
+}
