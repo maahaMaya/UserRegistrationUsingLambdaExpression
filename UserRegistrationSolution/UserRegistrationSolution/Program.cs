81ba89723bc55214fa220817d9b93230a898e111
SOURAV <devbhsourav@gmail.com>
2022-12-20 10:48:20 +0530

    [sourav]add : phoneNumber validator

--- UserRegistrationSolution/UserRegistrationSolution/Program.cs
+++ UserRegistrationSolution/UserRegistrationSolution/Program.cs
@@ -7,24 +7,26 @@ namespace UserRegistrationSolution
         string firstName = "Sourav";
         string lastName = "Kumar";
         string email = "Souravkumar@giet.edu.in";
+        string phoneNumber = "91 1234567890";
+  
         static void Main(string[] args)
         {
             Program p = new Program();
 
             //WithLambdaExpression
             UserRegistrationWithLambdaExpression userRegistrationWithLambdaExpression = new UserRegistrationWithLambdaExpression();
-            if (userRegistrationWithLambdaExpression.RegexCheck(p.firstName, p.lastName, p.email))
-                Console.WriteLine("Match found for the first name, last name and email.");
+            if (userRegistrationWithLambdaExpression.RegexCheck(p.firstName, p.lastName, p.email, p.phoneNumber))
+                Console.WriteLine("Match found for the first name, last name, email and phoneNumber.");
             else
-                Console.WriteLine("Match not found for the first name, last name and email.");
+                Console.WriteLine("Match not found for the first name, last name, email and phoneNumber.");
             Console.WriteLine();
 
             //WithoutLambdaExpression
             UserRegistrationWithoutLambdaExpression userRegistrationWithoutLambdaExpression = new UserRegistrationWithoutLambdaExpression();
-            if (userRegistrationWithoutLambdaExpression.RegexCheck(p.firstName, p.lastName, p.email))
-                Console.WriteLine("Match found for the first name, last name and email.");
+            if (userRegistrationWithoutLambdaExpression.RegexCheck(p.firstName, p.lastName, p.email, p.phoneNumber))
+                Console.WriteLine("Match found for the first name, last name, email and phoneNumber.");
             else
-                Console.WriteLine("Match not found for the first name, last name and email.");
+                Console.WriteLine("Match not found for the first name, last name, email and phoneNumber.");
         }
     }
 }
--- UserRegistrationSolution/UserRegistrationSolution/UserRegistrationWithoutLambdaExpression.cs
+++ UserRegistrationSolution/UserRegistrationSolution/UserRegistrationWithoutLambdaExpression.cs
@@ -12,9 +12,10 @@ namespace UserRegistrationSolution
         const string FIRST_NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
         const string LAST_NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
         const string EMAIL_PATTERN = "^[0-9a-zA-Z]+[.+-_]{0,1}[0-9a-zA-Z]+[@][a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,3}){0,1}";
-        public bool RegexCheck(string firstName, string lastName, string email)
+        const string PHONE_PATTERN = "^[0-9]{2}[ ]{1}[0-9]{10}";
+        public bool RegexCheck(string firstName, string lastName, string email, string phoneNumber)
         {
-            return (Regex.IsMatch(firstName, FIRST_NAME_PATTERN) && Regex.IsMatch(lastName, LAST_NAME_PATTERN) && Regex.IsMatch(email, EMAIL_PATTERN)) ? true : false;
+            return (Regex.IsMatch(firstName, FIRST_NAME_PATTERN) && Regex.IsMatch(lastName, LAST_NAME_PATTERN) && Regex.IsMatch(email, EMAIL_PATTERN) && Regex.IsMatch(phoneNumber, PHONE_PATTERN))? true : false;
         }
     }
 }
